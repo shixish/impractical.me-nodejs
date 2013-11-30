d620e1f64b1d8a5da5669ac8b6788b28e264c033
Andrew <shixish@gmail.com>
2013-11-30 02:38:31 -1000

    about to clean out extra code. was able to make menu system use ejs directly

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
 var express = require('express'),
     app = express(),
     engine = require('ejs-locals'),
+    ejs = require('ejs'),
     routes = require('./routes'),
     util = require('util'),
-    path = require('path');
+    path = require('path'),
+    fs = require('fs');
     //now = require('now'), //http://nowjs.com/
     //lessMiddleware = require('less-middleware'); //https://github.com/emberfeather/less.js-middleware
 
 
-var pub_dir = path.join(__dirname, 'public');
+var pub_dir = path.join(__dirname, 'public'),
+    views_dir = path.join(__dirname, 'views');
 // Configuration
 app.configure(function(){
   //app.set('views', __dirname + '/views');
@@ -20,12 +23,18 @@ app.configure(function(){
   // use ejs-locals for all ejs templates:
   app.engine('ejs', engine);
   
-  app.set('views',__dirname + '/views');
+  app.set('views', views_dir);
   app.set('view engine', 'ejs'); // so you can render('index')
+  //app.set('view options', {
+  //    open: '{{',
+  //    close: '}}'
+  //});
   
   app.use(app.router);
   
   app.use(express.static(pub_dir));
+  
+  app.set('title', 'impractical.me');
 
   //app.use(lessMiddleware({
   //  src: __dirname + '/public',
@@ -52,14 +61,55 @@ app.get('/about', function(request, response) {
   response.render('about.ejs', {title:'About Me', breadcrumb:['home']});
 });
 
-app.locals.menu = {
-  'about': 'About',
-  'derp': 'Herp'
+//var menu_items = [
+//  {
+//    uri:'about',
+//    title:'About'
+//  },{
+//    //uri:'resume',
+//    title:'Resume',
+//    children: [
+//      {
+//        uri:'thing',
+//        title:'Koalafications'
+//      }
+//    ]
+//  },
+//];
+
+
+var menu_items = {
+  'About':'about',
+  'Resume': {
+    'Koalafications': 'coolbeans'
+  },
 };
 
-app.locals.someHelper = function(name) {
-  return ("hello " + name);
+app.locals.menu_items = menu_items;
+
+function generate_menu(items) {
+  var ret = '';
+  for (var label in items) {
+    var value = items[label];
+    if (typeof value == 'object') {
+      ret += '<li class="dropdown">';
+        ret += '<a href="#" class="dropdown-toggle" data-toggle="dropdown">'+label+' <b class="caret"></b></a>';
+        ret += '<ul class="dropdown-menu">';
+          for (var inner_label in value) {
+            var inner_value = value[inner_label];
+            ret += '<li><a href="'+inner_value+'">'+inner_label+'</a></li>\n';
+          }
+          ret += generate_menu(value);
+        ret += '</ul>';
+      ret += '</li>';
+    }else{
+      ret += '<li><a href="'+value+'">'+label+'</a></li>\n';
+    }
+  }
+  return ret;
 }
+
+app.locals.generate_menu = generate_menu;
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
