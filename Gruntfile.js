b87775c4271b14f7759ba0695c093ff29e056fb0
Andrew <shixish@gmail.com>
2013-11-30 04:30:26 -1000

    compressed css

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt) {
     recess: {
       dist: {
         options: {
-          compile: true
+          compile: true,
+          compress: true
         },
         files: {
           'public/css/screen.css': [
